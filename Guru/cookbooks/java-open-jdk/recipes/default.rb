f4489c404de89a0aee74113e5a6a23fb60e2f92f
Jonathan RAMIER <jonathan.ramier@aramisauto.com>
2014-06-24 18:52:50 +0200

    ajout des utilisateur sudoer pour les resources chef

--- Guru/cookbooks/java-open-jdk/recipes/default.rb
+++ Guru/cookbooks/java-open-jdk/recipes/default.rb
@@ -8,16 +8,20 @@
 #
 
 execute "add-repository" do
-	command "add-apt-repository ppa:webupd8team/java"
+	command "sudo add-apt-repository ppa:webupd8team/java"
+	user "vagrant"
 	action :run
+	notifies :run, "execute[apt-get-update]", :immediately
 end
 
 execute "apt-get-update" do
-	command "apt-get update"
-	action :run
+	command "sudo apt-get update"
+	user "vagrant"
+	action :nothing
 end
 
 execute "install-oracle-java" do
-	command "apt-get install oracle-java8-installer"
+	command "sudo apt-get install oracle-java8-installer"
+	user "vagrant"
 	action :run
 end
